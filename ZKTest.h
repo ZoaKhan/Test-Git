ca6ac4af7a751a4aff98957e968f7a76169c2e0f
ZoaKhan <zk>
2014-05-22 14:49:35 -0700

    testing testing 123

---
+++ ZKTest.h
@@ -0,0 +1,13 @@
+//
+//  ZKTest.h
+//  Git Test
+//
+//  Created by Denise Choi on 5/22/14.
+//  Copyright (c) 2014 Zoa Khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZKTest : NSObject
+
+@end
---
+++ ZKTest.m
@@ -0,0 +1,13 @@
+//
+//  ZKTest.m
+//  Git Test
+//
+//  Created by Denise Choi on 5/22/14.
+//  Copyright (c) 2014 Zoa Khan. All rights reserved.
+//
+
+#import "ZKTest.h"
+
+@implementation ZKTest
+
+@end
